ae821c35126efc2e05f65b100af92a1f4d3a6cad
parad <parad@Ryzen>
2024-04-13 20:42:39 -0300

    Avance del ejercicio 11 y 3

--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio11/Ejercicio11.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio11/Ejercicio11.java
@@ -6,7 +6,7 @@ public class Ejercicio11 {
 		int i = 2;
         int j = 40;
 
-        for (; j >= 0; j -= 3, i += 2) {
+        for (; j >= 0; j -= 1, i += 1) {
             System.out.print((i * j) + " ");
         }
     }
--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio3/Ejercicio3.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio3/Ejercicio3.java
@@ -1,10 +1,29 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
-	}
+        // solicita al usuario que ingrese un número entero
+        System.out.print("Ingrese un número entero: ");
+        int numero = scanner.nextInt();
 
+        // verifica si el número ingresado es par o impar
+        if (numero % 2 == 0) {
+            System.out.println("El número ingresado es par.");
+            int resultado = numero * 3;
+            System.out.println("El triple del número es: " + resultado);
+        } else {
+            // si es impar mostrará el doble del número
+            System.out.println("El número ingresado es impar.");
+            int resultado = numero * 2;
+            System.out.println("El doble del número es: " + resultado);
+        }
+        
+        // se cierra el scanner para evitar fugas
+        scanner.close();
+    }
 }
