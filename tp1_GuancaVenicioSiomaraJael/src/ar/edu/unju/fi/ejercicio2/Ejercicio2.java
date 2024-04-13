5b2dbcab0d4a8062c7b52eae63fad05f0ad79d2a
parad <parad@Ryzen>
2024-04-13 01:40:33 -0300

    Commit del ejercicio número 2, primera parte.

--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio2/Ejercicio2.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio2/Ejercicio2.java
@@ -1,9 +1,51 @@
 package ar.edu.unju.fi.ejercicio2;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        
+        // declaración de las variables que usaremos
+        String nombrePais;
+        int edadPersona;
+        double alturaEdificio;
+        double precioProducto;
+        String numeroTelefono;
+        double coseno;
+
+        // aquí solicitaremos el ingreso de los datos
+        System.out.println("Ingrese el nombre de un país:");
+        nombrePais = scanner.nextLine();
+
+        System.out.println("Ingrese la edad de una persona:");
+        edadPersona = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Ingrese la altura de un edificio:");
+        alturaEdificio = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Ingrese el precio de un producto de supermercado:");
+        precioProducto = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Ingrese el número de teléfono:");
+        numeroTelefono = scanner.nextLine();
+
+        coseno = Math.cos(0.5);
+        
+        // exponemos por pantalla los datos ingresados
+        System.out.println("Datos ingresados son: ");
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edadPersona);
+        System.out.println("Altura del edificio: " + alturaEdificio);
+        System.out.println("Precio del producto: " + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Cálculo de coseno de 0.5: " + coseno);
+        
+        scanner.close();
 
 	}
 
