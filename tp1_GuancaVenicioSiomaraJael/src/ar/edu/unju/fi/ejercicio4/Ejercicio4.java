e8a7cdd836c972dfaa99be1bc3cdf3e378503d37
parad <parad@Ryzen>
2024-04-13 21:13:00 -0300

    Ejercicio 4, correciones previas y actualización.

--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio4/Ejercicio4.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio4/Ejercicio4.java
@@ -1,10 +1,31 @@
 package ar.edu.unju.fi.ejercicio4;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
-	}
+        // se solicita al usuario que ingrese un número que esté dentro del rango [0,10]
+        int numero;
+        do {
+            System.out.print("Ingrese un número entre 0 y 10: ");
+            numero = scanner.nextInt();
+        } while (numero < 0 || numero > 10);
 
+        // calcula el factorial del número utilizando un bucle while
+        int factorial = 1;
+        int contador = 1;
+        while (contador <= numero) {
+            factorial *= contador;
+            contador++;
+        }
+
+        // muestra el resultado por consola
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+        
+        // cerramos el scanner para evitar fugas
+        scanner.close();
+    }
 }
