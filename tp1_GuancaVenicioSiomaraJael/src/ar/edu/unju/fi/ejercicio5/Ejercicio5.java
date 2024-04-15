8a6be2ee9d5072f630a2e8f9521195e704ce161c
parad <parad@Ryzen>
2024-04-15 20:49:40 -0300

    Ejercicio 5 completo.

--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio5/Ejercicio5.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio5/Ejercicio5.java
@@ -1,10 +1,28 @@
 package ar.edu.unju.fi.ejercicio5;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Scanner scanner = new Scanner(System.in);
+        
+        // solicita al usuario que ingrese por consola un número entre 1 y 9
+        System.out.println("Por favor, ingresa un número entero entre 1 y 9:");
+        int numero = scanner.nextInt();
+        
+        // acá validamos que el número esté dentro del rango
+        if (numero < 1 || numero > 9) {
+            System.out.println("El número ingresado no está dentro del rango válido.");
+            
+        }
+        
+        // mostraMOS la tabla de multiplicar del número ingresado
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        
+        scanner.close();
+    }
 }
