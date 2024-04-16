e0328fe233761b6956104e0c0bd1f032f2b82368
parad <parad@Ryzen>
2024-04-15 22:30:36 -0300

    Corrección de detalles del ejercicio 7-

--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio7/Main.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio7/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-        // Solicitar datos para un nuevo empleado
+        // solicitamos los datos para un nuevo empleado
         System.out.println("Ingrese el nombre del empleado:");
         String nombre = scanner.nextLine();
         System.out.println("Ingrese el legajo del empleado:");
@@ -16,17 +16,17 @@ public class Main {
         System.out.println("Ingrese el salario del empleado:");
         double salario = Double.parseDouble(scanner.nextLine());
 
-        // Crear objeto Empleado utilizando el constructor parametrizado
+        // creamos el objeto Empleado utilizando el constructor parametrizado
         Empleado empleado = new Empleado(nombre, legajo, salario);
 
-        // Mostrar datos del empleado antes del aumento
+        // mostramps los datos del empleado antes del aumento
         System.out.println("Datos del empleado antes del aumento:");
         empleado.mostrarDatos();
 
-        // Dar aumento al salario del empleado
+        // el aumento al salario del empleado
         empleado.darAumento();
 
-        // Mostrar datos del empleado después del aumento
+        // datos del empleado después del aumento
         System.out.println("Datos del empleado después del aumento:");
         empleado.mostrarDatos();
 
--- tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
+++ tp1_GuancaVenicioSiomaraJael/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
@@ -5,11 +5,11 @@ public class Empleado {
     private int legajo;
     private double salario;
 
-    // Constantes
+    // constantes
     private static final double SALARIO_MINIMO = 210000.00;
     private static final double AUMENTO_POR_MERITOS = 20000.00;
 
-    // Constructor parametrizado
+    // los constructor parametrizado
     public Empleado(String nombre, int legajo, double salario) {
         this.nombre = nombre;
         this.legajo = legajo;
@@ -20,14 +20,14 @@ public class Empleado {
         }
     }
 
-    // Método para mostrar los datos del empleado
+    // método para mostrar los datos del empleado
     public void mostrarDatos() {
         System.out.println("Nombre del empleado: " + nombre);
         System.out.println("Legajo: " + legajo);
         System.out.println("Salario $: " + salario);
     }
 
-    // Método para dar un aumento al salario del empleado
+    // método para dar un aumento al salario del empleado
     public void darAumento() {
         salario += AUMENTO_POR_MERITOS;
     }
